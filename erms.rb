79e5255b69a4d479606dfd9f0aad7bbf833773de
Kosuke Tanabe <nabeta@fastmail.fm>
2015-09-05 03:27:08 +0900

    DOAIのファイルを読み込めるようにした

--- erms.rb
+++ erms.rb
@@ -4,7 +4,7 @@ require 'optparse'
 require 'library_stdnums'
 require 'sqlite3'
 
-opts = ARGV.getopts('l:', 'k:')
+opts = ARGV.getopts('l:', 'k:', 'd:')
 
 db = SQLite3::Database.new("erms.db")
 sql = <<EOS
@@ -12,7 +12,8 @@ CREATE TABLE IF NOT EXISTS journals (
   issn varchar(8),
   price integer,
   title varchar(255),
-  print_issn varchar(8)
+  print_issn varchar(8),
+  open_access integer
 );
 EOS
 db.execute(sql)
@@ -31,7 +32,7 @@ csv.each do |line|
   end
 end
 
-# KBARTのファイルを追加
+# KBARTのファイルを読み込む
 if opts['k']
   kbart = CSV.table(File.open(opts['k']), col_sep: "\t")
   kbart.each do |line|
@@ -48,6 +49,27 @@ if opts['k']
         print_issn,
         issn
       )
+      puts "#{issn}\tfound KBART record"
+    end
+  end
+end
+
+# DOAJのジャーナルリストを読み込む
+if opts['d']
+  doaj = CSV.table(File.open(opts['d']))
+  doaj.each do |line|
+    issn = StdNum::ISSN.normalize(line[:journal_eissn_online_version])
+    result = db.execute(
+      'SELECT issn FROM journals WHERE issn = ?',
+      issn
+    )
+    unless result.empty?
+      db.execute(
+        'UPDATE journals SET open_access = ? WHERE issn = ?',
+        1,
+        issn
+      )
+      puts "#{issn}\topen access"
     end
   end
 end
