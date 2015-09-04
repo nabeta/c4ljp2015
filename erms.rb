a037b1c5af97b09ae898a3d84cd452e5b25597ba
Kosuke Tanabe <nabeta@fastmail.fm>
2015-09-05 03:11:59 +0900

    ISSNが正しいかどうかをチェックするようにした

--- erms.rb
+++ erms.rb
@@ -1,11 +1,18 @@
 #!/usr/bin/ruby
 require 'csv'
 require 'optparse'
+require 'library_stdnums'
 
 opts = ARGV.getopts('l:')
 
 csv = CSV.table(open(opts['l']), col_sep: "\t")
 csv.each do |line|
-  puts "#{line[:issn]}\t#{line[:price]}"
+  # ISSNが正しければ処理を続け、不正ならエラーで終了する
+  if StdNum::ISSN.valid?(line[:issn])
+    # ハイフンを削除する
+    issn = StdNum::ISSN.normalize(line[:issn])
+    puts "#{line[:issn]}\t#{line[:price]}"
+  else
+    puts "Invalid ISSN: #{line[:issn]}"
+  end
 end
-
